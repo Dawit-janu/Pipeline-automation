c3afb63c4c1af2d31c7d635ce6db92826b2ad7ae
Dawit Janu Irawan <dawitjanuirawan@gmail.com>
2026-02-18 11:31:02 +0700

    Send Notif report to Discord

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  // ✅ Tambahan mochawesome reporter
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "results",
+    overwrite: false,
+    html: false,
+    json: true
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-  videoCompression:15,
+
+  videoCompression: 15,
 });
