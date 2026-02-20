a583de759bbcaebbe0b07e8df5327b05bec600d2
Dawit Janu Irawan <dawitjanuirawan@gmail.com>
2026-02-20 16:42:25 +0700

    allure report

--- cypress.config.js
+++ cypress.config.js
@@ -2,25 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 
-  // ✅ TAMBAHKAN DI SINI (LEVEL ATAS)
-  projectId: "wipkva",
-
-  // ✅ Tambahan mochawesome reporter
-  reporter: "mochawesome",
-  reporterOptions: {
-    reportDir: "results",
-    overwrite: false,
-    html: false,
-    json: true
-  },
-
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+
+      // ✅ WAJIB untuk generate allure-results
+      require("@shelex/cypress-allure-plugin/writer")(on, config);
+
       return config;
     },
   },
-  video: true,
-  screenshotOnRunFailure: true,
+
   videoCompression: 15,
 });
