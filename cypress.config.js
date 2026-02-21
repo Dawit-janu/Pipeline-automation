6a794391eba874399eaa152f0f144829fca1c897
Dawit Janu Irawan <dawitjanuirawan@gmail.com>
2026-02-21 17:04:20 +0700

    Dashboard cloud dan allure

--- cypress.config.js
+++ cypress.config.js
@@ -3,18 +3,18 @@ const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
 
-  // ✅ Cypress Cloud - Project ID
+  // ✅ Cypress Cloud Project ID
   projectId: "wipkva",
 
   e2e: {
     setupNodeEvents(on, config) {
-      // ✅ Allure plugin writer
+      // ✅ Allure plugin — generate allure-results per test
       allureWriter(on, config);
       return config;
     },
   },
 
-  video: true,
-  screenshotOnRunFailure: true,
+  video: true,                    // ✅ Video direkam → tersimpan di Cypress Cloud
+  screenshotOnRunFailure: true,   // ✅ Screenshot saat gagal → tersimpan di Cypress Cloud
   videoCompression: 15,
 });
