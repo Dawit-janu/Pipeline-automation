1fa8cebcdffd2c6a1cb7550a1fda1579a32df240
Dawit Janu Irawan <dawitjanuirawan@gmail.com>
2026-02-22 16:40:52 +0700

    Add TS-Seva

---
+++ cypress/e2e/TS-Seva/TC01-Seva-Dash.cy.js
@@ -0,0 +1,10 @@
+Cypress.on('uncaught:exception',(err,runnable) =>
+{
+    return false
+})
+describe('Search Dashboard', () => {
+    it('Search Dashboard',() =>{
+        //cy.visit('https://www.demoblaze.com/')
+        cy.get('#cat').contains('CATEGORIES')
+    })
+})
